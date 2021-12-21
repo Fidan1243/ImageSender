0944add865a753545cb4f33e9e8dd670af6f8bec
Iman_xz90 <Iman_xz90@itstep.edu.az>
2021-12-21 12:00:34 +0400

    Add project files.

---
+++ WpfApp1/ViewModels/MainViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Models;
+
+namespace WpfApp1.ViewModels
+{
+    public class MainViewModel:BaseViewModel
+    {
+        private ObservableCollection<PhotoUser> allPhotos;
+
+        public ObservableCollection<PhotoUser> AllPhotos
+        {
+            get { return allPhotos; }
+            set { allPhotos = value;OnPropertyChanged(); }
+        }
+
+        public MainViewModel()
+        {
+            AllPhotos = new ObservableCollection<PhotoUser>();
+            var rc = Task.Run(() =>
+            {
+                Reciever();
+            });
+            Task.WaitAll(rc);
+        }
+
+
+        public void Reciever()
+        {
+            var ipAddress = IPAddress.Parse("10.1.18.52");
+            var port = 27001;
+            using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                var ep = new IPEndPoint(ipAddress, port);
+                socket.Bind(ep);
+
+                socket.Listen(10);
+                Console.WriteLine($"Listening on {socket.LocalEndPoint}");
+
+
+                while (true)
+                {
+                    var client = socket.Accept();
+                    Task.Run(() =>
+                    {
+
+                        Console.WriteLine($"{client.RemoteEndPoint}  connected  . . . ");
+                        var length = 0;
+                        var bytes = new byte[1024];
+                        
+                        do
+                        {
+                            length = client.Receive(bytes);
+                            var msg = Encoding.UTF8.GetString(bytes, 0, length);
+                            AllPhotos.Add(new PhotoUser
+                            {
+                                UserName = client.RemoteEndPoint.ToString(),
+                                ImagePath = msg
+                            });
+                            if (msg == "ok")
+                            {
+                                client.Shutdown(SocketShutdown.Both);
+                                client.Dispose();
+                                break;
+                            }
+
+
+                        } while (true);
+                    });
+                }
+
+            }
+        }
+    }
+}
---
+++ WpfApp3/ViewModels/MainViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp3.Commands;
+
+namespace WpfApp3.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public RelayCommand SelectICommand { get; set; }
+        public RelayCommand SendCommand { get; set; }
+        public bool SendClickChecker { get; set; } = false;
+        public string ImagePath { get; set; } = "";
+
+        public MainViewModel()
+        {
+
+        }
+
+        public void Send()
+        {
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            var ipAddress = IPAddress.Parse("10.1.18.52");
+            var port = 27001;
+            var ep = new IPEndPoint(ipAddress, port);
+
+            try
+            {
+                socket.Connect(ep);
+                if (socket.Connected)
+                {
+                    Console.WriteLine("Connected to the server . . .");
+                    while (true)
+                    {
+                        if (SendClickChecker)
+                        {
+                            if (ImagePath != "")
+                            {
+
+                            var bytes = Encoding.UTF8.GetBytes(ImagePath);
+                            socket.Send(bytes);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Can not connect to the server . . .");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Can not connect to the server . . .");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
